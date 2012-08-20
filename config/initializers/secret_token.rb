8631535d1eb52b19cbb923375ae4ca1c6a68bc47
Felipe Moyano <gafemoyano@gmail.com>
2012-08-20 15:24:00 -0500

    agregar archivos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeraEntrega::Application.config.secret_token = '80397141b3575c3688c914ff7da764897accf4040cea6c04e9b3cda1e7c5b81914310fa4e88223676b9e1a55c47dba0df5a1d94a756c73237241229aacf91d03'
